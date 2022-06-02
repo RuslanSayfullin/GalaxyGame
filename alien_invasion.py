6e0101ab15739979755f7f6c7211628f23e728dc
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-06-02 11:45:41 +0500

    Создание класса Settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,15 +1,14 @@
 import sys
 
 import pygame
-
+from settings import Settings
 
 def run_game():
-    # Инициализирует игру и создает объект экрана.
+    # Инициализирует pygame, settings и объект экрана.
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    # Назначение цвета фона.
-    bg_color = (230, 230, 230)
     # Запуск основного цикла игры.
     while True:
         # Отслеживание событий клавиатуры и мыши.
@@ -17,7 +16,8 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
             # При каждом проходе цикла перерисовывается экран.
-            screen.fill(bg_color)
+            screen.fill(ai_settings.bg_color)
+
         # Отображение последнего прорисованного экрана.
         pygame.display.flip()
     run_game()
