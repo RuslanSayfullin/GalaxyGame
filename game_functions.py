d74fc7ed2a60e192f70029a2ea6537aecbb5d86f
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-06-06 09:51:29 +0500

    Создание функции update_bullets()

--- game_functions.py
+++ game_functions.py
@@ -45,3 +45,13 @@ def update_screen(ai_settings, screen, ship, bullets):
     ship.blitme()
     # Отображение последнего прорисованного экрана.
     pygame.display.flip()
+
+
+def update_bullets(bullets):
+    """Обновляет позиции пуль и уничтожает старые пули."""
+    # Обновление позиций пуль.
+    bullets.update()
+    # Удаление пуль, вышедших за край экрана.
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
