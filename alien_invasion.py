ab782be3148e33670c6adfe844157aeb077f571e
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-06-08 10:19:01 +0500

    Снижение флота и смена направления

--- alien_invasion.py
+++ alien_invasion.py
@@ -34,6 +34,6 @@ def run_game():
         ship.update()
         bullets.update()
         gf.update_bullets(bullets)
-        gf.update_aliens(aliens)
+        gf.update_aliens(ai_settings, aliens)
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
     run_game()
