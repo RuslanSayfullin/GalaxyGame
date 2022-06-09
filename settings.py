6f68dd93d6a7de513fd12f7552c751ff0deefd3b
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-06-09 09:51:21 +0500

    Ускорение пуль

--- settings.py
+++ settings.py
@@ -7,7 +7,8 @@ class Settings():
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         # Параметры пули
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 3
+        self.bul
         self.fleet_drop_speed = 10
         # fleet_direction = 1 обозначает движение вправо; а -1 - влево.
         self.fleet_direction = 1
