897246b9ce9c9b70dce58ef521979d01f62e9c61
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-06-09 09:46:18 +0500

    Восстановление флота

--- game_functions.py
+++ game_functions.py
@@ -56,7 +56,7 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
     pygame.display.flip()
 
 
-def update_bullets(aliens, bullets):
+def update_bullets(ai_settings, screen, ship, aliens, bullets):
     """Обновляет позиции пуль и уничтожает старые пули."""
     # Обновление позиций пуль.
     bullets.update()
@@ -67,6 +67,11 @@ def update_bullets(aliens, bullets):
         # Проверка попаданий в пришельцев.
         # При обнаружении попадания удалить пулю и пришельца.
         collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+        if len(aliens) == 0:
+            # Уничтожение существующих пуль и создание нового флота.
+            bullets.empty()
+            create_fleet(ai_settings, screen, ship, aliens)
+
 
 def get_number_aliens_x(ai_settings, alien_width):
     """Вычисляет количество пришельцев в ряду."""
