c8460c12e2dbfba4cc8f3150e137dbe09f61d952
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-06-09 09:56:41 +0500

    Рефакторинг update_bullets()

--- game_functions.py
+++ game_functions.py
@@ -64,13 +64,17 @@ def update_bullets(ai_settings, screen, ship, aliens, bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
-        # Проверка попаданий в пришельцев.
-        # При обнаружении попадания удалить пулю и пришельца.
-        collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
-        if len(aliens) == 0:
-            # Уничтожение существующих пуль и создание нового флота.
-            bullets.empty()
-            create_fleet(ai_settings, screen, ship, aliens)
+            check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+
+
+def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
+    """Обработка коллизий пуль с пришельцами."""
+    # Удаление пуль и пришельцев, участвующих в коллизиях.
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+    if len(aliens) == 0:
+        # Уничтожение существующих пуль и создание нового флота.
+        bullets.empty()
+        create_fleet(ai_settings, screen, ship, aliens)
 
 
 def get_number_aliens_x(ai_settings, alien_width):
