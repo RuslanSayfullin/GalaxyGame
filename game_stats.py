9778c1ced55a4b9e7c7ec250898b059811829e2e
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-06-10 10:41:19 +0500

    Конец игры

--- game_stats.py
+++ game_stats.py
@@ -5,8 +5,9 @@ class GameStats():
         """Инициализирует статистику."""
         self.ai_settings = ai_settings
         self.reset_stats()
+        # Игра Alien Invasion запускается в активном состоянии.
+        self.game_active = True
 
     def reset_stats(self):
         """Инициализирует статистику, изменяющуюся в ходе игры."""
         self.ships_left = self.ai_settings.ship_limit
-        
